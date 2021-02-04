1e754c8457595087b818d0b32e0c4dda7ecc717d
Amir Behmaram <amirbehmaram4@gmail.com>
2021-02-03 20:57:26 -0800

    More Heroku thingies
    Signed-off-by: Amir Behmaram <amirbehmaram4@gmail.com>

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import os
+#!/usr/bin/env python
 from flask import Flask
+app = Flask(__name__)
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_mapping(
-        SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key'
-    )
+@app.route('/')
 
-    return app
+def hello_world():
+    return 'Hello, World!'
