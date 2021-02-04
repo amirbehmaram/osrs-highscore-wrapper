47e11f9a639addcdf178a2e94e5d81b94747f29d
Amir Behmaram <amirbehmaram4@gmail.com>
2021-02-03 21:45:13 -0800

    IN progress
    Signed-off-by: Amir Behmaram <amirbehmaram4@gmail.com>

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
-from flask import Flask
-app = Flask(__name__)
+from app import app
 
-@app.route('/')
-
-def hello_world():
-    return 'Hello, World!'
+if __name__ == "__main__":
+    app.run()
